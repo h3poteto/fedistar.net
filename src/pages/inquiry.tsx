65ed359634310a7898c9892815ba9660e586a79f
AkiraFukushima <h3.poteto@gmail.com>
2023-01-17 20:38:19 +0900

    Create top page

---
+++ src/pages/inquiry.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Inquiry() {
+  return (
+    <>
+      <Head>
+        <title>Inqiury - Fedistar</title>
+        <meta
+          name="description"
+          content="Multi-column Mastodon and Pleroma client for desktop"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>inquiry</main>
+    </>
+  );
+}
