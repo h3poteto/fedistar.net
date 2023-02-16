963da5322a49a2dedef3715a67c76f07b0f1fa0b
AkiraFukushima <h3.poteto@gmail.com>
2023-02-17 02:00:20 +0900

    Implement footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,24 +10,57 @@ const footer = () => {
           marginTop: '8vh'
         }}
       >
-        <FlexboxGrid justify="center" align="middle" style={{ height: 120 }}>
-          <FlexboxGrid.Item colspan={4} style={{ textAlign: 'center' }}>
-            <a href="https://pleroma.io/users/h3poteto" target="_blank" rel="noopener noreferrer">
-              Pleroma
-            </a>
+        <FlexboxGrid justify="center" align="top" style={{ height: 'auto', paddingTop: 24, paddingBottom: 12 }}>
+          <FlexboxGrid.Item style={{ textAlign: 'left', width: '180px' }}>
+            <h5 style={{ paddingBottom: 12 }}>Fedistar</h5>
+            <p style={{ lineHeight: '24px' }}>
+              <a href="https://github.com/h3poteto/fedistar" target="_blank" rel="noopener noreferrer">
+                Source Code
+              </a>
+            </p>
+            <p style={{ lineHeight: '24px' }}>
+              <Link href="/privacy_policy">Privacy Policy</Link>{' '}
+            </p>
+
+            <p style={{ lineHeight: '24px' }}>
+              <Link href="/inquiry">Contact Us</Link>{' '}
+            </p>
           </FlexboxGrid.Item>
-          <FlexboxGrid.Item colspan={4} style={{ textAlign: 'center' }}>
-            <a href="https://fedibird.com/@h3poteto" target="_blank" rel="noopener noreferrer">
-              Mastodon
-            </a>
+          <FlexboxGrid.Item style={{ textAlign: 'left', width: '180px' }}>
+            <h5 style={{ paddingBottom: 12 }}>Whalebird</h5>
+            <p style={{ lineHeight: '24px' }}>
+              <a href="https://whalebird.social" target="_blank" rel="noopener noreferrer">
+                Whalebird
+              </a>
+            </p>
+            <p style={{ lineHeight: '24px' }}>
+              <a href="https://whalebird.social/desktop/contents/downloads" target="_blank" rel="noopener noreferrer">
+                Download
+              </a>
+            </p>
+            <p style={{ lineHeight: '24px' }}>
+              <a href="https://github.com/h3poteto/whalebird-desktop" target="_blank" rel="noopener noreferrer">
+                Source Code
+              </a>
+            </p>
+            <p style={{ lineHeight: '24px' }}>
+              <a href="https://whalebird.social/desktop/privacy_policy" target="_blank" rel="noopener noreferrer">
+                Privacy Policy
+              </a>
+            </p>
           </FlexboxGrid.Item>
-          <FlexboxGrid.Item colspan={4} style={{ textAlign: 'center' }}>
-            <a href="https://github.com/h3poteto/fedistar" target="_blank" rel="noopener noreferrer">
-              GitHub
-            </a>
-          </FlexboxGrid.Item>
-          <FlexboxGrid.Item colspan={4} style={{ textAlign: 'center' }}>
-            <Link href="/inquiry">Contact Us</Link>
+          <FlexboxGrid.Item style={{ textAlign: 'left', width: '180px' }}>
+            <h5 style={{ paddingBottom: 12 }}>About Us</h5>
+            <p style={{ lineHeight: '24px' }}>
+              <a href="https://pleroma.io/users/h3poteto" target="_blank" rel="noopener noreferrer">
+                Pleroma
+              </a>
+            </p>
+            <p style={{ lineHeight: '24px' }}>
+              <a href="https://fedibird.com/@h3poteto" target="_blank" rel="noopener noreferrer">
+                Mastodon
+              </a>
+            </p>
           </FlexboxGrid.Item>
         </FlexboxGrid>
         <FlexboxGrid justify="center" align="middle" style={{ height: 48 }}>
